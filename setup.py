bc7b668e32bb493c23edccc09f01159cbc89ac6b
kai studio <diekunnazar@gmail.com>
2024-12-11 22:07:41 +0200

    remake V1

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cortelxlib', 
-    version='0.1.0',   
+    name="cortelxlib",  
+    version="0.1",  
+    description="Library for CortelXLoader project",  
+    long_description=open("README.md").read(),  
+    long_description_content_type="text/markdown", 
+    author="kai studio",  
+    author_email="diekunnazar@gmail.com",  
+    url="https://github.com/kai-studios/cortelxlib",  
     packages=find_packages(),  
-    install_requires=[ 
-        'requests',  
+    install_requires=[],  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later", 
+        "Operating System :: OS Independent",
     ],
-    author='kai studio',
-    author_email='diekunnazar@gmail.com',
-    description='A library for managing modular systems, loading models dynamically, and providing an isolated execution environment for secure operations.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/kai-studios/cortelxlib',
-    classifiers=[ 
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6', 
 )
